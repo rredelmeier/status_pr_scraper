69c456669afa6a0bd20e6daa44dcf335ccfe80ea
rredelmeier <r.redelmeier@gmail.com>
2017-10-07 04:30:28 -0400

    Fixing barrels of diesel+gasoline and storing numbers as floats
    seperated diesel+gasoline into two seperate keys; numbers are either
    type float or None (in the case of curr/total values for cards that
    don't report such).

--- status_pr_scraper.py
+++ status_pr_scraper.py
@@ -18,7 +18,18 @@ def get_data():
             return tag['class'] in (['text-muted'],['text-muted', 'capitalize'],['text-muted','info','font-medium-2'],['text-muted','pb-0'],["font-large-2","text-bold-300","info"])
         else:
             return False
-    
+
+    def str_to_float(data_str):
+        if data_str != "":
+
+            data_int = float(data_str.replace(',','').replace('%','').strip())
+            if '%' in data_str:
+                data_int = data_int*100
+            return data_int
+        else:
+            return None
+
+
     card_names = ['gas','supermarket','AEE','telecommunication','antenna','tower','shelter','refugee', 'pet','hospital','dialysis','pharmacy','port',
                     'container','bank','cooperatives','ATMs','barrel','bread','ama','federal_mail']
 
@@ -35,16 +46,16 @@ def get_data():
         for i,data_point in enumerate(list(card.find_all(is_data_point))):
             line = data_point.text
             if 'Barriles' in line: weird_oil_card=True
-            
+
             if not weird_oil_card:
                 if i == 0:
                     big_val = line
                 elif i == 1 and data_point['class'] != ["font-large-2","text-bold-300","info"]:
                     if line.find('de') != -1 and 'Diálisis' not in line and 'Servicio de Telecomunicaciones' not in line:
                         # Handle gasoline/supermarkets/containers/bank branches/postal offices
-                        name = line[:re.search('( [1-9])',line).start()-1]
-                        curr = line[re.search('( [1-9])',line).start()+1:line.find('de ')-1]
-                        total = line[line.find('de ')+3:]
+                        name = line[:re.search('( [1-9])',line).start()-1].strip()
+                        curr = line[re.search('( [1-9])',line).start()+1:line.find('de ')-1].strip()
+                        total = line[line.find('de ')+3:].strip()
                     else:
                         # Handle the rest
                         name = line[:line.find('  ')].strip()
@@ -69,6 +80,16 @@ def get_data():
                 elif i == 5:
                     note = line[len('Nota: '):].strip()
 
-        ret[card_names[j]]={'Reported Value':big_val,'Current':curr,'Total':total,'Last Update':last_update,'Source':source,'Note':note}
-
+        if card_names[j]!='barrel':
+            big_val = str_to_float(big_val)
+            curr = str_to_float(curr)
+            total = str_to_float(total)
+            ret[card_names[j]]={'Reported Value':big_val,'Current':curr,'Total':total,'Last Update':last_update,'Source':source,'Note':note}
+        else:
+            ret[card_names[j]+'_diesel']={'Reported Value':str_to_float(big_val['Diesel']),'Current':curr,'Total':total,'Last Update':last_update,'Note':note}
+            ret[card_names[j]+'_gasoline']={'Reported Value':str_to_float(big_val['Gasoline']),'Current':curr,'Total':total,'Last Update':last_update,'Note':note}
     return ret
+
+test_dict = get_data()
+for key in test_dict.keys():
+    print(key+": "+str(test_dict[key]))
